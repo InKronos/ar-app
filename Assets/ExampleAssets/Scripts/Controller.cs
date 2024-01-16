0875c3e280063dbe3fcb5018447fe3e43886b98d
Błażej Kołodziej <blazej4099@gmail.com>
2024-01-16 19:08:15 +0100

    Controlling helicopters with a virtual joystick

--- Assets/ExampleAssets/Scripts/Controller.cs
+++ Assets/ExampleAssets/Scripts/Controller.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    [SerializeField] private float helicopterSpeed = 0.5f;
     [SerializeField] private float rotorSpeedModifier = 10f;
     [SerializeField] private Transform rotorsTransform;
+
+    private FixedJoystick joystick;
+    private Rigidbody rigidbody;
 
     private bool rotate = false;
 
     public void toggleRotation() {
         rotate = !rotate;
     }
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        joystick = FindObjectOfType<FixedJoystick>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
         if (rotate) rotorsTransform.Rotate(Vector3.up * 50f * rotorSpeedModifier);
+
+        float xVal = joystick.Horizontal;
+        float zVal = joystick.Vertical;
+
+        Vector3 movement = new Vector3(xVal, 0, zVal);
+        rigidbody.velocity = movement * helicopterSpeed;
+
+        if(xVal !=0 && zVal !=0) 
+        { 
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(xVal, zVal)*Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
     }
 
 }
